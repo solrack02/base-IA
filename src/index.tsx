2bfa4e6dde76f16ee8ff4f090f755ef8d4f387a7
Carlos A. <carlosvsk@gmail.com>
2024-10-08 14:09:36 -0300

    Commit generated by Flaxboll.

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,88 @@
-        // ---------- import React Packs
-        import React from 'react';
-        import * as RN from 'react-native';
 
-        // ---------- import Variables Pack
-        import { create } from 'zustand';
 
-        // ---------- import Local Tools
+          // ---------- import React Packs
+          import React from 'react';
+          import * as RN from 'react-native';
 
-        // ---------- set Caps Inputs
-        const currRoute = ''
+          // ---------- import Variables Pack
+          import { create } from 'zustand';
 
-        let args:any = []
+          // ---------- import Local Tools
+          import { mapElements } from './tools/base/project/mapElements';
+          import * as functions from './tools/base/functions';
+          import * as Elements from './tools/base/Elements';
+          import { Project } from './tools/base/project';
+          import * as jsvals from './tools/base/jsvals';
+          import { props } from './tools/base/props';
+          import * as customs from './tools/customs';
+          import * as stls from './tools/base/stls';
+          import { tools } from './tools';
 
-        const screens = []
+          // ---------- set Caps Inputs
+          const currRoute = 'sc2';
 
-        const initCt = () => ({});
-        const initObj = initCt();
+          let args: any = [];
 
-        const arrInitFuncs = [];
+          const screens = [
+            // 
 
-        export const useRoutes = create(() => ({ currRoute }));
-        export const useData = create(() => initObj);
+        (...args: any) => <Elements.Screen3 pass={{
+          pathScreen:"sc2",
 
-        // ---------- set Main Component
-        export const Router = () => {
-          return (<RN.Text>Base Project...</RN.Text>);
-      };
+          styles:[
+              {
+                backgroundColor: '#101',
+                justifyContent: 'center',
+                alignItems: 'center',
+                height: '100%',
+                width: '100%',
+              }
+              ],
+
+          screenElements:[() => {
+                  const textStyle = {
+                    fontSize: 20,
+                    color: '#fff2',
+                    textAlign:'center',
+                    maxWidth: 200,
+                    // maxWidth: '200px'<= #ATTENTION: Native ERROR! No string!
+                  };
+
+                  return (
+                    <RN.Text style={textStyle}>
+                      {'Adicione Elementos nessa tela!'}
+                    </RN.Text>);
+                }],
+
+          startFunctions:[()=>{}],
+
+          args,
+        }}/>
+          ];
+
+          const initCt = () => ({
+            // true: "true"
+          });
+          const initObj = initCt();
+          // console.log(initObj);
+
+          const arrInitFuncs = [
+            // ()=>{}
+          ];
+
+          export const useRoutes = create(() => ({ currRoute }));
+          export const useData = create(() => initObj);
+
+          // ---------- set Main Component
+          export const Router = () => {
+            return (
+              <Project
+                configData={{
+                  screens,
+                  initCt,
+                  arrInitFuncs,
+                }}
+              />
+            );
+          };
+        
